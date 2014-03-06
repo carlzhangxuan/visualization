b23a94db54ee84e1a7eb64e1ba906059b42fb175
zhang <carlzhangxuan@gmail.com>
2014-03-06 11:54:59 +0800

    r

--- One_2_One_2nd_new/data/makejson.py
+++ One_2_One_2nd_new/data/makejson.py
@@ -6,18 +6,32 @@
 
 fn = 'jiudianyuding_agoda_7_p.path'
 
+
+head = """{"nodes":["""
+mid = """],
+"links":["""
+end = """]}"""
 lin_con = []
 for line in open(fn):
 	for word in line.strip('\n').split('\t'):
 		if word not in lin_con:
 			lin_con.append(word)
+print head
 my_dict = {}			
 for (num, word) in enumerate(lin_con):
 	my_dict[word] = num
-	print num, word
-
+	print '{"name":"'+word+'", "color":"white"},'
+print  mid
+my_dict_2 = {}
 for line in open(fn):
 	#print [my_dict[key] for key in line.strip('\n').split('\t')]
 	case = line.strip('\n').split('\t')
 	for i in xrange(len(case) - 1):
-		print my_dict[case[i]], my_dict[case[i + 1]]
+		if (my_dict[case[i]], my_dict[case[i + 1]]) not in my_dict_2:
+			my_dict_2[(my_dict[case[i]], my_dict[case[i + 1]])] = 1
+		else:
+			my_dict_2[(my_dict[case[i]], my_dict[case[i + 1]])] += 1
+
+for key in my_dict_2:
+	print '{"source":'+str(key[0])+',"target":'+str(key[1])+',"value":'+str(my_dict_2[key])+'},'
+print end
