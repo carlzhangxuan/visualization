28e9f9246da484f7777212b5a4dd73baaa9371a3
zhang <carlzhangxuan@gmail.com>
2014-09-01 17:44:32 +0800

    front
    front

--- chord/read_json.py
+++ chord/read_json.py
@@ -2,4 +2,7 @@ import json
 
 text = json.load(open('dota.json'))
 
-print text['nodes']
+for node in text['nodes']:
+	print node[u'name'].encode('utf8')
+for link in text['links']:
+	print link['source'], link['target']
